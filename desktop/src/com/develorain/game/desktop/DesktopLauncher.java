6b5919c539fe538bb76673909ac58bdb7e25bf4c
Develorain <feomsometog@gesmgo.com>
2015-09-21 21:04:55 -0400

    Initial Commit

---
+++ desktop/src/com/develorain/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.develorain.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.develorain.game.Illumination;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Illumination(), config);
+	}
+}
