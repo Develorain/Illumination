be6c266d97a342fe8fae95e3e913b11a2424f025
Develorain <feomsometog@gesmgo.com>
2015-09-22 21:31:27 -0400

    Implemented Box2D Rendering

--- core/src/com/develorain/game/Screens/PlayScreen.java
+++ core/src/com/develorain/game/Screens/PlayScreen.java
@@ -5,32 +5,69 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.develorain.game.Illumination;
+
+import java.awt.*;
 
 public class PlayScreen implements Screen {
     private Illumination game;
     private OrthographicCamera cam;
     private Viewport gamePort;
 
+    // Tiled map variables
     private TmxMapLoader mapLoader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
+    // Box2D variables
+    private World world;
+    private Box2DDebugRenderer b2dr; // the outlines of the shapes and stuff
+
+
     public PlayScreen(Illumination game) {
         this.game = game;
         cam = new OrthographicCamera();
-        gamePort = new FitViewport(Illumination.V_WIDTH, Illumination.V_HEIGHT, cam); // possibly change to ScreenViewport
+        //gamePort = new FitViewport(Illumination.V_WIDTH, Illumination.V_HEIGHT, cam); // possibly change to ScreenViewport
+        gamePort = new ScreenViewport(cam);
 
         mapLoader = new TmxMapLoader();
         map = mapLoader.load("level1.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
 
         cam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0, -9.8f), true);
+        b2dr = new Box2DDebugRenderer();
+
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        for(MapObject object : map.getLayers().get(1).getObjects().getByType(RectangleMapObject.class)) {
+             Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
     }
 
     public void update(float dt) {
@@ -49,6 +86,8 @@ public class PlayScreen implements Screen {
 
         renderer.render();
 
+        b2dr.render(world, cam.combined);
+
         game.batch.setProjectionMatrix(cam.combined);
     }
 
@@ -59,6 +98,14 @@ public class PlayScreen implements Screen {
 
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             cam.position.x -= 100 * dt;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            cam.position.y += 100 * dt;
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            cam.position.y -= 100 * dt;
         }
     }
 
