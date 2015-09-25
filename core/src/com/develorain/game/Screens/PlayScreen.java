4421816a9e3204c30bea3af2fbbcdd48a645176e
Develorain <feomsometog@gesmgo.com>
2015-09-24 22:34:22 -0400

    Re-factoring code and Basic Implementation of Player Sprite

--- core/src/com/develorain/game/Screens/PlayScreen.java
+++ core/src/com/develorain/game/Screens/PlayScreen.java
@@ -5,24 +5,23 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.develorain.game.Illumination;
 import com.develorain.game.Sprites.Cubey;
-
+import com.develorain.game.Tools.B2WorldCreator;
 
 
 public class PlayScreen implements Screen {
+    // Reference to the game, used to set Screens
     private Illumination game;
+
+    // Basic PlayScreen variables
     private OrthographicCamera cam;
     private Viewport gamePort;
 
@@ -33,55 +32,52 @@ public class PlayScreen implements Screen {
 
     // Box2D variables
     private World world;
-    private Box2DDebugRenderer b2dr; // the outlines of the shapes and stuff
+    private Box2DDebugRenderer b2dr;
     private Cubey player;
 
-
     public PlayScreen(Illumination game) {
+        // Set game as class variable
         this.game = game;
+
+        // Initialize camera/viewport variable
         cam = new OrthographicCamera();
         gamePort = new FitViewport(Illumination.V_WIDTH / Illumination.PPM, Illumination.V_HEIGHT / Illumination.PPM, cam); // possibly change to ScreenViewport
-        //gamePort = new ScreenViewport(cam);
+        //gamePort = new ScreenViewport(cam); temporary code
 
+        // Initialize tiled map variables
         mapLoader = new TmxMapLoader();
         map = mapLoader.load("level1.tmx");
         renderer = new OrthogonalTiledMapRenderer(map, 1 / Illumination.PPM);
 
+        // Set initial camera position to center of the screen (pretty much useless)
         cam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
 
+        // Initialize world
         world = new World(new Vector2(0, -9.8f), true);
         b2dr = new Box2DDebugRenderer();
 
+        // Initialize player
         player = new Cubey(world);
 
-        BodyDef bdef = new BodyDef();
-        PolygonShape shape = new PolygonShape();
-        FixtureDef fdef = new FixtureDef();
-        Body body;
-
-        for(MapObject object : map.getLayers().get(1).getObjects().getByType(RectangleMapObject.class)) {
-             Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set((rect.getX() + rect.getWidth() / 2) / Illumination.PPM, (rect.getY() + rect.getHeight() / 2) / Illumination.PPM);
-
-            body = world.createBody(bdef);
-
-            shape.setAsBox(rect.getWidth() / 2 / Illumination.PPM, rect.getHeight() / 2 / Illumination.PPM);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
+        // Initializes the collision of the static tiles (ground)
+        new B2WorldCreator(world, map);
     }
 
     public void update(float dt) {
+        // Handle input
         handleInput(dt);
 
+        // Sets the world frames to 60 FPS
         world.step(1 / 60f, 6, 2);
 
+        // Centers the camera on the player (requires refactoring in the future, incorporate lerping to target, add new package of utilities)
         cam.position.x = player.b2body.getPosition().x;
         cam.position.y = player.b2body.getPosition().y;
 
+        // Updates the camera
         cam.update();
+
+        // Sets the tiled map renderer to render only what is on screen or in camera view
         renderer.setView(cam);
     }
 
@@ -89,29 +85,40 @@ public class PlayScreen implements Screen {
     public void render(float dt) {
         update(dt);
 
+        // Clears screen
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Renders the tiled map
         renderer.render();
 
+        // Renders the box2D debug renderer (lines)
         b2dr.render(world, cam.combined);
 
+        // Draws player
+        player.drawPlayer(game.batch);
+
+        // Sets projection of the batch to the camera's matrices
         game.batch.setProjectionMatrix(cam.combined);
     }
 
     public void handleInput(float dt) {
+        // Runs if right is pressed or held
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2) {
             player.b2body.applyLinearImpulse(new Vector2(0.1f, 0), player.b2body.getWorldCenter(), true);
         }
 
+        // Runs if left is pressed or held
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2) {
             player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0), player.b2body.getWorldCenter(), true);
         }
 
+        // Runs if up is pressed
         if(Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
             player.b2body.applyLinearImpulse(new Vector2(0, 4f), player.b2body.getWorldCenter(), true);
         }
 
+        // Runs if down is pressed
         if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
 
         }
@@ -144,6 +151,9 @@ public class PlayScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        map.dispose();
+        renderer.dispose();
+        world.dispose();
+        b2dr.dispose();
     }
 }
--- core/src/com/develorain/game/Sprites/Cubey.java
+++ core/src/com/develorain/game/Sprites/Cubey.java
@@ -1,29 +1,59 @@
 package com.develorain.game.Sprites;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 import com.develorain.game.Illumination;
 
 public class Cubey extends Sprite {
     public World world;
     public Body b2body;
+    public Sprite boxSprite;
+    Array<Body> tmpBodies;
 
     public Cubey(World world) {
         this.world = world;
-        defineCubey();
+        definePlayer();
+        tmpBodies = new Array<Body>();
     }
 
-    public void defineCubey() {
+    public void definePlayer() {
+        boxSprite = new Sprite(new Texture("cubey.png"));
+        boxSprite.setSize(0.5f, 0.5f);
+
         BodyDef bdef = new BodyDef();
         bdef.position.set(100 / Illumination.PPM, 100 / Illumination.PPM);
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
+        b2body.setUserData(boxSprite);
+
         FixtureDef fdef = new FixtureDef();
-        CircleShape shape = new CircleShape();
-        shape.setRadius(5 / Illumination.PPM);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(boxSprite.getTexture().getWidth() / Illumination.PPM, boxSprite.getTexture().getHeight() / Illumination.PPM);
 
         fdef.shape = shape;
         b2body.createFixture(fdef);
+    }
+
+    public void drawPlayer(Batch batch) {
+        batch.begin();
+        world.getBodies(tmpBodies);
+        for(Body body: tmpBodies) {
+            if(body.getUserData() != null && body.getUserData() instanceof Sprite) {
+                Sprite sprite = (Sprite) body.getUserData();
+
+                // Sets the texture to the center of the player
+                sprite.setPosition(body.getPosition().x - boxSprite.getTexture().getWidth() / Illumination.PPM,
+                        body.getPosition().y - boxSprite.getTexture().getHeight() / Illumination.PPM);
+
+                sprite.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
+                sprite.draw(batch);
+            }
+        }
+        batch.end();
     }
 }
