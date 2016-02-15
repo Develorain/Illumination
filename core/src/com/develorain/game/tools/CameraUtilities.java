0a86ec892cb10690b04a7b01dff2afa5fe4c6749
Develorain <sogmsotgmseot@foemasf.com>
2016-02-14 20:35:54 -0500

    Added screen shake functionality

--- core/src/com/develorain/game/tools/CameraUtilities.java
+++ core/src/com/develorain/game/tools/CameraUtilities.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
+import static com.develorain.game.Illumination.PPM;
+
 public class CameraUtilities {
     public static void lerpToTarget(Camera cam, Vector2 target) {
         Vector3 position = cam.position;
@@ -11,6 +13,16 @@ public class CameraUtilities {
         position.y = cam.position.y + (target.y - cam.position.y) * 0.1f;
         cam.position.set(position);
 
+        cam.update();
+    }
+
+    // add interpolated camera shake so it looks more natural
+    public static void shake(Camera cam) {
+        Vector3 position = cam.position;
+        float random = (float) Math.random() * 3 - 3;
+        position.x = cam.position.x + random / PPM;
+        float random1 = (float) Math.random() * 3 - 3;
+        position.y = cam.position.y + random1 / PPM;
         cam.update();
     }
 
